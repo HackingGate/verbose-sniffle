9772ae84a187d30c251f8b9be75d48a526c327a3
HackingGate <8541644+HackingGate@users.noreply.github.com>
2023-05-21 14:45:25 +0900

    Create project with Xcode 14.2

---
+++ PictureViewer/PictureViewer/PictureViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictureViewerApp.swift
+//  PictureViewer
+//
+//  Created by H G on 2023/05/20.
+//
+
+import SwiftUI
+
+@main
+struct PictureViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
